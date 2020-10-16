846cee1d78ed9cf52636cde9d39ab10ca6e7b680
misha <sedachev.m1@gmail.com>
2020-10-16 13:29:48 +0400

    refact: modals state

--- src/components/pages/UserArticle/UserArticle.tsx
+++ src/components/pages/UserArticle/UserArticle.tsx
@@ -9,14 +9,12 @@ import Container from '../../ui/Container/Container';
 import DeleteArticleModal from '../../modals/DeleteArticleModal/DeleteArticleModal';
 import EditArticleModal from '../../modals/EditModal/EditArticleModal';
 import Loader from '../../ui/Loader/Loader';
-import Moment from 'react-moment';
 import Title from '../../ui/Title/Title';
-import User from '../../ui/User/User';
-import UserAvatar from '../../ui/User/UserAvatar/UserAvatar';
+import UserInfo from '../../ui/User/UserInfo/UserInfo';
 import Wrapper from '../../ui/Wrapper/Wrapper';
 import { getArticle } from '../../../services/articleService/articleService';
 import { useAuth } from '../../Contexts/AuthContext';
-import { useLocation } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import { useQuery } from 'react-query';
 
 type UserArticle = {
@@ -29,15 +27,21 @@ type UserArticle = {
 };
 
 const UserArticle: React.FC = (): JSX.Element => {
-  const {
-    state: { slug },
-  } = useLocation<{ slug: string }>();
+  const { slug } = useParams<{ slug: string }>();
   const { data, isLoading } = useQuery(['article', slug], getArticle);
 
-  const [show, setShow] = useState(false);
-  const [showDeleteArticleModal, setShowDeleteArticleModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const { user: authUser } = useAuth();
+
+  const handleSetShowDeleteModal = () => {
+    setShowDeleteModal(true);
+  };
+
+  const handleSetShowEditModal = () => {
+    setShowEditModal(true);
+  };
 
   return (
     <div className="user-article">
@@ -48,7 +52,6 @@ const UserArticle: React.FC = (): JSX.Element => {
           <Banner backgroundColor="#333">
             <Container>
               <Wrapper style={{ display: 'flex', flexDirection: 'column' }}>
-                <h1 className="user-article__title"></h1>
                 <Wrapper
                   style={{
                     display: 'flex',
@@ -56,20 +59,13 @@ const UserArticle: React.FC = (): JSX.Element => {
                     marginTop: '20px',
                   }}
                 >
-                  <UserAvatar
-                    className="user-avatar__feed"
+                  <UserInfo
+                    classNameUserDate="user-date--white"
+                    classNameUserName="username--white"
                     username={data.author.username}
                     image={data.author.image}
+                    createdAt={data.createdAt}
                   />
-                  <div>
-                    <User
-                      className="user--white"
-                      username={data.author.username}
-                    />
-                    <Moment className="user-article--white" format="LL HH:mm">
-                      {}
-                    </Moment>
-                  </div>
                 </Wrapper>
                 {authUser.username === data.author.username ? (
                   <Wrapper
@@ -81,13 +77,13 @@ const UserArticle: React.FC = (): JSX.Element => {
                     }}
                   >
                     <Button
-                      onClick={() => setShowDeleteArticleModal(true)}
+                      onClick={handleSetShowDeleteModal}
                       className="button__remove"
                     >
                       Delete article
                     </Button>
                     <Button
-                      onClick={() => setShow(true)}
+                      onClick={handleSetShowEditModal}
                       className="button__edit"
                     >
                       Edit article
@@ -103,24 +99,23 @@ const UserArticle: React.FC = (): JSX.Element => {
           </Container>
 
           <CommentList slug={slug} />
+          {showEditModal ? (
+            <EditArticleModal
+              slug={data.slug}
+              title={data.title}
+              body={data.body}
+              description={data.description}
+              setShow={setShowEditModal}
+            />
+          ) : null}
+          {showDeleteModal ? (
+            <DeleteArticleModal
+              slug={slug}
+              setShowDeleteArticleModal={setShowDeleteModal}
+            />
+          ) : null}
         </div>
       )}
-      {show ? (
-        <EditArticleModal
-          slug={data.slug}
-          title={data.title}
-          body={data.body}
-          description={data.description}
-          setShow={setShow}
-        />
-      ) : null}
-
-      {showDeleteArticleModal ? (
-        <DeleteArticleModal
-          slug={slug}
-          setShowDeleteArticleModal={setShowDeleteArticleModal}
-        />
-      ) : null}
     </div>
   );
 };
