ff696881c8827791a04d82a09fbaa4058668c672
misha <sedachev.m1@gmail.com>
2020-10-20 18:09:40 +0400

    chore: added types deleteArticle func

--- src/components/modals/DeleteCommentModal/DeleteCommentModal.tsx
+++ src/components/modals/DeleteCommentModal/DeleteCommentModal.tsx
@@ -1,10 +1,15 @@
 import '../../../assets/scss/modal.scss';
 
+import {
+  Comment,
+  DeleteComment,
+} from '../../../services/commentService/commentService.types';
 import React, { FC } from 'react';
 import { queryCache, useMutation } from 'react-query';
 
 import Backdrop from '../../ui/Backdrop/Backdrop';
 import Button from '../../ui/Button/Button';
+import { ErrorRollback } from '../../../types/error.type';
 import Modal from '../Modal/Modal';
 import { VscClose } from 'react-icons/vsc';
 import Wrapper from '../../ui/Wrapper/Wrapper';
@@ -12,18 +17,16 @@ import { deleteComment } from '../../../services/commentService/commentService';
 
 type Props = {
   setShow: (value: boolean) => void;
-  show: boolean;
   slug: string;
   id: number;
 };
 
-const DeleteCommentModal: FC<Props> = ({
-  setShow,
-  slug,
-  id,
-  show,
-}): JSX.Element => {
-  const [mutate, { isLoading }] = useMutation(deleteComment, {
+const DeleteCommentModal: FC<Props> = ({ setShow, slug, id }): JSX.Element => {
+  const [mutate, { isLoading }] = useMutation<
+    Comment,
+    ErrorRollback,
+    DeleteComment
+  >(deleteComment, {
     onMutate: ({ slug, id }) => {
       queryCache.cancelQueries(['comments', slug]);
 
@@ -36,13 +39,12 @@ const DeleteCommentModal: FC<Props> = ({
       return () =>
         queryCache.setQueryData(['comments', slug], previousComments);
     },
-    onError: (rollback: () => void) => rollback(),
+    onError: (rollback) => rollback(),
     onSettled: () => {
       queryCache.invalidateQueries(['comments', slug]);
     },
   });
 
-  if (!show) return null;
   return (
     <>
       <Backdrop setShow={setShow} />
